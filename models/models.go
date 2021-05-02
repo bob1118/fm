7a7cab6481a327b533d29b4a1b83132ba71579ef
bob <chenbob@hotmail.com>
2021-05-02 18:54:38 +0800

    models freeswitch cdr fix

--- models/models.go
+++ models/models.go
@@ -9,7 +9,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var db, pgdb *sqlx.DB
+var db, cdrdb *sqlx.DB
 
 //init function.
 func init() {
@@ -36,29 +36,33 @@ func InitFreeswitch(strcon string) {
 	//freeswitch database.
 	pgsqlInitFreeswitch(strcon)
 
+	//cdr tables.
+	strcdr := "host=127.0.0.1 dbname=freeswitch user=fsdba password=fsdba sslmode=disable"
+	pgsqlInitFreeswitchCDR(strcdr)
+
 	//freeswitch tables.
 	s := fmt.Sprintf("user=%s password=%s host=%s dbname=%s sslmode=disable", fmconfig.CFG.Database.User, fmconfig.CFG.Database.Password, fmconfig.CFG.Database.Host, fmconfig.CFG.Database.Name)
 	if db, err = openPgsql(s); err != nil {
-		pgsqlInitFreeswitchCDR()
 		pgsqlInitFreeswitchAccounts()
 		pgsqlInitFreeswitchGateways()
 		pgsqlInitFreeswitchE164s()
 	}
 }
 
+//pgsqlInitFreeswitch function.
 func pgsqlInitFreeswitch(strcon string) {
 	var err error
 	var isFound bool
-	if pgdb, err = openPgsql(strcon); err != nil {
-		log.Println(err.Error())
+	if pgdb, e := openPgsql(strcon); e != nil {
+		log.Println(e.Error())
 	} else {
-		if err = pgdb.Get(&isFound, "select count(1)!=0 as isFound from pg_user where usename =$1", "fsdba"); err != nil {
+		if err = pgdb.Get(&isFound, "select count(1)!=0 as isFound from pg_user where usename =$1", fmconfig.CFG.Database.User); err != nil {
 			log.Println(err.Error())
 		} else {
 			if !isFound { //create user.
 				pgdb.MustExec(USER_CREATE)
 			}
-			if err = pgdb.Get(&isFound, "select count(1)!=0 as isFound from pg_database where datname=$1", "freeswitch"); err != nil {
+			if err = pgdb.Get(&isFound, "select count(1)!=0 as isFound from pg_database where datname=$1", fmconfig.CFG.Database.Name); err != nil {
 				log.Println(err.Error())
 			} else {
 				if !isFound { //create db.
@@ -71,31 +75,35 @@ func pgsqlInitFreeswitch(strcon string) {
 	}
 }
 
-//pgsqlInitFreeswitchCDR function
-func pgsqlInitFreeswitchCDR() {
+//pgsqlInitFreeswitchCDR function.
+func pgsqlInitFreeswitchCDR(strcdr string) {
 	var err error
 	var isFound bool
-
-	if err = db.Get(&isFound, "select count(1)!=0 as isFound from pg_tables where tablename =$1", "table cdr_table_a_leg"); err != nil {
+	if cdrdb, err = openPgsql(strcdr); err != nil {
 		log.Println(err.Error())
 	} else {
-		if !isFound {
-			db.MustExec(CDR_ALEG)
+		if err = cdrdb.Get(&isFound, "select count(1)!=0 as isFound from pg_tables where tablename =$1", "cdr_table_a_leg"); err != nil {
+			log.Println(err.Error())
+		} else {
+			if !isFound {
+				cdrdb.MustExec(CDR_ALEG)
+			}
 		}
-	}
-	if err = db.Get(&isFound, "select count(1)!=0 as isFound from pg_tables where tablename =$1", "table cdr_table_b_leg"); err != nil {
-		log.Println(err.Error())
-	} else {
-		if !isFound {
-			db.MustExec(CDR_BLEG)
+		if err = cdrdb.Get(&isFound, "select count(1)!=0 as isFound from pg_tables where tablename =$1", "cdr_table_b_leg"); err != nil {
+			log.Println(err.Error())
+		} else {
+			if !isFound {
+				cdrdb.MustExec(CDR_BLEG)
+			}
 		}
-	}
-	if err = db.Get(&isFound, "select count(1)!=0 as isFound from pg_tables where tablename =$1", "table cdr_table_both"); err != nil {
-		log.Println(err.Error())
-	} else {
-		if !isFound {
-			db.MustExec(CDR_BOTH)
+		if err = cdrdb.Get(&isFound, "select count(1)!=0 as isFound from pg_tables where tablename =$1", "cdr_table_both"); err != nil {
+			log.Println(err.Error())
+		} else {
+			if !isFound {
+				cdrdb.MustExec(CDR_BOTH)
+			}
 		}
+		//closePgsql(cdrdb)
 	}
 }
 
--- models/sqldefine.go
+++ models/sqldefine.go
@@ -4,43 +4,49 @@ const USER_CREATE = "create user fsdba with password 'fsdba'"
 const DB_CREATE = "create database freeswitch with owner fsdba"
 const DBUSER_AUTH = "grant all privileges on database freeswitch to fsdba"
 
-const CDR_ALEG = `create table cdr_table_a_leg(
-	id						serial primary key,
-	CallId					uuid not null,
-	orig_id					uuid not null,
-	term_id					varchar,
-	ClientId				uuid,
-	IP						inet,
-	IPInternal				inet,
-	CODEC					varchar,
-	directGateway			inet,
-	redirectGateway			inet,
-	CallerID				varchar,
-	TelNumber				varchar,
-	TelNumberFull			varchar,
-	sip_endpoint_disposition	varchar,
-	sip_current_application		varchar
+const CDR_ALEG = `
+CREATE TABLE cdr_table_a_leg (
+	id serial NOT NULL,
+	callid uuid NOT NULL,
+	orig_id uuid NOT NULL,
+	term_id varchar NULL,
+	clientid uuid NULL,
+	ip inet NULL,
+	ipinternal inet NULL,
+	codec varchar NULL,
+	directgateway inet NULL,
+	redirectgateway inet NULL,
+	callerid varchar NULL,
+	telnumber varchar NULL,
+	telnumberfull varchar NULL,
+	sip_endpoint_disposition varchar NULL,
+	sip_current_application varchar NULL,
+	CONSTRAINT cdr_table_a_leg_pkey PRIMARY KEY (id)
 )`
-const CDR_BLEG = `create table cdr_table_b_leg(
-	id						serial primary key,
-	CallId					uuid not null,
-	orig_id					uuid not null,
-	term_id					varchar,
-	ClientId				uuid,
-	IP						inet,
-	IPInternal				inet,
-	CODEC					varchar,
-	directGateway			inet,
-	redirectGateway			inet,
-	CallerID				varchar,
-	TelNumber				varchar,
-	TelNumberFull			varchar,
-	sip_endpoint_disposition	varchar,
-	sip_current_application		varchar
+const CDR_BLEG = `
+CREATE TABLE cdr_table_b_leg (
+	id serial NOT NULL,
+	callid uuid NOT NULL,
+	orig_id uuid NOT NULL,
+	term_id varchar NULL,
+	clientid uuid NULL,
+	ip inet NULL,
+	ipinternal inet NULL,
+	codec varchar NULL,
+	directgateway inet NULL,
+	redirectgateway inet NULL,
+	callerid varchar NULL,
+	telnumber varchar NULL,
+	telnumberfull varchar NULL,
+	sip_endpoint_disposition varchar NULL,
+	sip_current_application varchar NULL,
+	CONSTRAINT cdr_table_b_leg_pkey PRIMARY KEY (id)
 )`
-const CDR_BOTH = `create table cdr_table_both(
-	id 		serial primary key,
-	CallId	uuid,
-	orig_id uuid,
-	Test_id varchar
+const CDR_BOTH = `
+CREATE TABLE cdr_table_both (
+	id serial NOT NULL,
+	callid uuid NULL,
+	orig_id uuid NULL,
+	test_id varchar NULL,
+	CONSTRAINT cdr_table_both_pkey PRIMARY KEY (id)
 )`
